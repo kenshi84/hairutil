0d8276e5546d389a15f63ef61a5355a550a161e3
Kenshi Takayama <kenshi84@gmail.com>
2025-08-29 18:48:10 +0900

    make it build again
    test/util.cpp: stop comparing strings directly within EXPECT_EQ by using str.compare()

--- test/util.cpp
+++ test/util.cpp
@@ -5,37 +5,37 @@
 TEST(util_trim_whitespaces, space) {
     const std::string str_in = "  a b c  ";
     const std::string str_out = util::trim_whitespaces(str_in);
-    EXPECT_EQ(str_out, "a b c");
+    EXPECT_EQ(str_out.compare("a b c"), 0);
 }
 
 TEST(util_trim_whitespaces, tab) {
     const std::string str_in = "\t\ta\tb\tc\t\t";
     const std::string str_out = util::trim_whitespaces(str_in);
-    EXPECT_EQ(str_out, "a\tb\tc");
+    EXPECT_EQ(str_out.compare("a\tb\tc"), 0);
 }
 
 TEST(util_replace_space_with_underscore, space) {
     const std::string str_in = "a b c";
     const std::string str_out = util::replace_space_with_underscore(str_in);
-    EXPECT_EQ(str_out, "a_b_c");
+    EXPECT_EQ(str_out.compare("a_b_c"), 0);
 }
 
 TEST(util_replace_space_with_underscore, tab) {
     const std::string str_in = "a\tb\tc";
     const std::string str_out = util::replace_space_with_underscore(str_in);
-    EXPECT_EQ(str_out, "a_b_c");
+    EXPECT_EQ(str_out.compare("a_b_c"), 0);
 }
 
 TEST(util_replace_space_with_underscore, newline) {
     const std::string str_in = "a\nb\nc";
     const std::string str_out = util::replace_space_with_underscore(str_in);
-    EXPECT_EQ(str_out, "a_b_c");
+    EXPECT_EQ(str_out.compare("a_b_c"), 0);
 }
 
 TEST(util_squalsh_underscores, test) {
     const std::string str_in = "a__b___c";
     const std::string str_out = util::squash_underscores(str_in);
-    EXPECT_EQ(str_out, "a_b_c");
+    EXPECT_EQ(str_out.compare("a_b_c"), 0);
 }
 
 TEST(util_lexical_cast, int) {
